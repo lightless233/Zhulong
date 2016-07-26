d390bd7be27e643c0536f1a43cecaefa0e972e02
lightless <lightless@foxmail.com>
2016-07-26 20:50:30 +0800

    init

--- Zhulong.py
+++ Zhulong.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # coding: utf-8
-# file: Test.py
-# time: 2016/7/26 20:48
+# file: Zhulong.py
+# time: 2016/7/26 20:47
 
 __author__ = "lightless"
 __email__ = "root@lightless.me"
