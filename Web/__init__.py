f1ab9b4870fbba13276153eff294d6884a80ba41
lightless <lightless@foxmail.com>
2016-08-07 20:18:00 +0800

    add some comment

--- Web/__init__.py
+++ Web/__init__.py
@@ -3,6 +3,7 @@
 # file: __init__.py
 # time: 16-8-7 16:12
 
+import os
 import sys
 
 from flask import Flask
@@ -12,13 +13,19 @@ __author__ = "lightless"
 __email__ = "root@lightless.me"
 
 
+# 设置编码为UTF-8
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-web = Flask(__name__)
+# 获取templates和static目录
+template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+
+# 初始化Flask app
+web = Flask(__name__, static_folder=static_folder, template_folder=template_folder)
 manager = Manager(web)
 manager.add_command("runserver", Server(host="0.0.0.0"))
 
-
+# 引入路由
 from Web.Controller.Frontend import IndexController
 
