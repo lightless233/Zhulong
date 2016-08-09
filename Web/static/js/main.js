1b29d48bc3f59c22979c11bb0b99a222a3ec5c8d
lightless <lightless@foxmail.com>
2016-08-09 21:12:56 +0800

    完成index

--- Web/static/js/main.js
+++ Web/static/js/main.js
@@ -3,5 +3,12 @@
  */
 
 $(document).ready(function () {
-    
+    $("a").click(function (event) {
+        event.preventDefault();
+    });
+
+    $("#banner h1").hide().fadeIn(1000);
+    $("#banner p").hide().fadeIn(1000);
+    $("#banner a").hide().fadeIn(1000);
+
 });
