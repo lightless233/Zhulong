cbf89f876403ba8b80649932e2036c992bbc30b1
lightless <lightless@foxmail.com>
2016-09-08 00:18:55 +0800

    完成add docker页面的前端

--- Web/Controller/API/APIController.py
+++ Web/Controller/API/APIController.py
@@ -7,13 +7,15 @@ from flask import jsonify, request
 from sqlalchemy import distinct
 
 from Web import web, db
-from Web.models import ZhulongOPSystem
+from Web.models import ZhulongOPSystem, ZhulongBaseComponents, ZhulongBaseComponentType
+from Utils.LoginRequire import login_required
 
 __author__ = "lightless"
 __email__ = "root@lightless.me"
 
 
 @web.route("/api/v1/get_op_systems", methods=["GET"])
+@login_required
 def v1_get_op_systems():
     ops = db.session.query(distinct(ZhulongOPSystem.op_name)).all()
     # 清除ops中的垃圾信息
@@ -25,6 +27,7 @@ def v1_get_op_systems():
 
 
 @web.route("/api/v1/get_op_system_versions", methods=["GET"])
+@login_required
 def v1_get_op_system_versions():
     """
     获取指定操作系统的版本信息
@@ -45,4 +48,54 @@ def v1_get_op_system_versions():
         versions.append(t)
     return jsonify(code=1001, message="query success.", versions=versions)
 
+
+@web.route("/api/v1/get_base_components", methods=['GET'])
+@login_required
+def v1_get_base_components():
+    """
+    获取基础组件的信息
+    :return: json
+    [
+        {
+            "com_type": "database",
+            "data": [
+                {"com_name": "MySQL", versions:["5.7", "5.6"]},
+                {"com_name": "SQLite", versions:["2.1", "3.0"]},
+            ],
+        },
+        ...
+    ]
+    """
+    result = db.session.query(
+        ZhulongBaseComponents.components_name,
+        ZhulongBaseComponents.components_version,
+        ZhulongBaseComponentType.type_name,
+    ).filter(
+        ZhulongBaseComponents.components_type == ZhulongBaseComponentType.id
+    ).all()
+    print result
+    # 整理数据
+    return_list = list()
+    for info in result:
+        # 检查return_list中是否存在对应类型的字典
+        found = False
+        for dd in return_list:
+            if dd.get("com_type") == info[2]:
+                found = True
+                # 找到了，直接向data中添加数据
+                # 检查dd中是否存在该类型的组件
+                f_found = False
+                for c in dd["data"]:
+                    if c.get("com_name") == info[0]:
+                        # 找到了，添加版本信息
+                        f_found = True
+                        c["versions"].append(info[1])
+                        break
+                if not f_found:
+                    dd["data"].append(dict(com_name=info[0], versions=[info[1]]))
+                break
+        else:
+            return_list.append(dict(com_type=info[2], data=[dict(com_name=info[0], versions=[info[1]])]))
+
+    return jsonify(code=1001, data=return_list)
 
--- Web/static/js/controllers.js
+++ Web/static/js/controllers.js
@@ -7,7 +7,7 @@ userHomeApp.controller("bodyController", function ($scope) {
 });
 
 userHomeApp.controller("addDockerController", function ($scope, $http, $log) {
-
+    // todo: 拆分这个controller
     // API的版本
     var API_VERSION = "v1";
 
@@ -18,35 +18,91 @@ userHomeApp.controller("addDockerController", function ($scope, $http, $log) {
         url: "/api/" + API_VERSION + "/get_op_systems"
     }).success(function (data) {
         if (data.code != 1001) {
+            // 状态码失败
             $scope.errorMessage = data.messag;
         } else {
+            // 正常
             $scope.OPSystemButtons = data.ops;
         }
+    }).error(function (data) {
+        $log.error("拉取系统镜像信息失败");
+        $scope.errorMessage = "Error while pulling operation system...";
+        return false;
     });
-    // $scope.OPSystemButtons = ["ubuntu", "centos", "debian"];
 
     // 操作系统的版本列表
     $scope.op_versions = {};
     $scope.selectedOPVersion = "";
 
+    // base components
+    $scope.selectedBaseCom = {};
+    $scope.baseComVersions = {};
+    // 用户选择的基础组件版本信息
+    $scope.userSelectComVersions = {};
+
+    // 获取基础组件的信息
+    // todo: 与上面的API合并，减少请求次数
+    $http({
+        method: "GET",
+        url: "/api/" + API_VERSION + "/get_base_components"
+    }).success(function (data) {
+        if (data.code != 1001) {
+            $scope.errorMessage = "Error while pull base components information...";
+            return false;
+        } else {
+            $scope.baseComs = data.data;
+            // 提取其中的versions信息
+            for(var i = 0; i < data.data.length; ++i) {
+                var t = data.data[i];
+                for (var j = 0; j < t.data.length; ++j) {
+                    $scope.baseComVersions[t.data[j].com_name] = t.data[j];
+                }
+            }
+        }
+        // $log.debug($scope.baseComVersions);
+    }).error(function (data) {
+        $log.error("拉取基础组件信息失败");
+        $scope.errorMessage = "Error while pulling base components...";
+        return false;
+    });
+
+    // 选择操作系统的按钮
     $scope.clickOPSystemButton = function (op) {
         $scope.activeButtonClass = op;
-        $log.debug(op);
-
         // 从后台获取操作系统版本信息
         $http({
             method: 'GET',
             url: "/api/" + API_VERSION + "/get_op_system_versions?op=" + op
-        }).success(function (data, header, config, status) {
-            $log.debug(data);
+        }).success(function (data) {
             if (data.code != 1001) {
                 $scope.errorMessage = data.message;
             } else {
                 $scope.op_versions = data.versions;
             }
-        }).error(function (data, header, config, status) {
+        }).error(function (data) {
             $log.error("拉取操作系统版本信息失败！");
-            $scope.errorMessage = "Error when getting op system..."
+            $scope.errorMessage = "Error when getting op system...";
+            return false;
         });
+    };
+
+    // 选择基础组件的按钮
+    $scope.clickBaseComBtn = function(baseCom) {
+        // 如果不存在就推进去，否则就删去
+        $log.debug($scope.selectedBaseCom[baseCom]);
+        if ($scope.selectedBaseCom[baseCom]) {
+            // 存在
+            delete $scope.selectedBaseCom[baseCom];
+        } else {
+            // 不存在，和版本信息一起push进去
+            $scope.selectedBaseCom[baseCom] = $scope.baseComVersions[baseCom];
+        }
+
+        $log.debug($scope.selectedBaseCom);
+    };
+
+    // 检查基础组件的按钮状态
+    $scope.chkBaseComBtn = function(baseCom) {
+        return $scope.selectedBaseCom[baseCom];
     }
 });
