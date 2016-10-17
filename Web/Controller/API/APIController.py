ea77306dbb943107c212c7d7a5dc7ebfc4d4f6ba
lightless <lightless@foxmail.com>
2016-10-17 22:05:14 +0800

    Add Create docker backend

--- Web/Controller/API/APIController.py
+++ Web/Controller/API/APIController.py
@@ -4,10 +4,10 @@
 # time: 2016/9/6 21:11
 
 from flask import jsonify, request
-from sqlalchemy import distinct
 
-from Web import web, db
+from Web import web, db, docker_client
 from Web.models import ZhulongSystemImages
+from Web.models import ZhulongUserContainers
 from Utils.LoginRequire import login_required
 
 __author__ = "lightless"
@@ -44,5 +44,41 @@ def api_get_info():
 @web.route("/api/v1/create_docker", methods=["POST"])
 @login_required
 def api_create_docker():
-    pass
+    print request.json
+    exposed_port = request.json.get("port", "")
+    version_id = request.json.get("version_id", "")
+
+    if version_id == "":
+        return jsonify(code=1004, message="系统版本选择有误")
+
+    # 检查version_id
+    system_image = ZhulongSystemImages.query.filter(ZhulongSystemImages.id == version_id).first()
+    if not system_image:
+        return jsonify(code=1004, message="系统版本选择有误")
+    print system_image
+
+    # 整理port
+    try:
+        ports = [int(p.strip()) for p in exposed_port.split(",")]
+    except ValueError:
+        return jsonify(code=1004, message="端口填写有误")
+    print ports
+
+    # 默认开启22端口
+    if 22 not in ports:
+        ports.append(22)
+
+    # 调用docker API跑起container
+    image_name = system_image.image_name
+    container_id = docker_client.create_container(
+        image=image_name,
+        ports=ports,
+        host_config=docker_client.create_host_config(
+            publish_all_ports=True
+        )
+    )
+    print container_id
+    # 插入到数据库中
+
+    return "123"
 
